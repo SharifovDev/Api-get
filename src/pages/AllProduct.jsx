d18b43eeddd8bd7fa5410dfcd63c381bc1b3acdf
rrt78537-dotcom <rrt78537@gmail.com>
2026-03-14 12:20:30 +0500

    add cards

--- src/pages/AllProduct.jsx
+++ src/pages/AllProduct.jsx
@@ -13,18 +13,31 @@ function AllProduct() {
       console.log(err);
     }
   };
-  useEffect(()=>{
-    fetchProduct()
-  }, [])
+  useEffect(() => {
+    fetchProduct();
+  }, []);
   return (
     <div>
       <Sidebar />
-      <div className="border-2 max-w-7xl ml-auto m-6">
-        {products.map((product,index)=>{
-          <div>
-            <h1>{product.name}</h1>
+      <div className="border-2 lg:max-w-7xl md:max-w-xl ml-auto m-6 grid grid-cols-2 gap-4">
+        {products.map((product, index) => (
+          <div key={index} className="border-2 border-red-500 hover:">
+            <h1>{product?.id}</h1>
+            <h1>{product?.name}</h1>
+            <h1>{product?.data?.Capacity}</h1>
+            <h1>{product?.data?.color}</h1>
+            <h1>{product?.data?.price}</h1>
+            <h1>{product?.data?.generation}</h1>
+            <h1>{product?.data?.year}</h1>
+            <h1>{product?.data?.["CPU model"]}</h1>
+            <h1>{product?.data?.["Hard disk size"]}</h1>
+            <h1>{product?.data?.["capacity GB"]}</h1>
+            <h1>{product?.data?.["Case Size"]}</h1>
+            <h1>{product?.data?.["Strap Colour"]}</h1>
+            <h1>{product?.data?.["Screen size"]}</h1>
+            <h1>{product?.data?.Description}</h1>
           </div>
-        })}
+        ))}
       </div>
     </div>
   );
