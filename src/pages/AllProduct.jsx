ba84d04cbd3aeca4b399326d4f6d038e5b1624c8
rrt78537-dotcom <rrt78537@gmail.com>
2026-03-14 09:13:50 +0500

    pul objects

--- src/pages/AllProduct.jsx
+++ src/pages/AllProduct.jsx
@@ -1,25 +1,31 @@
 import { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
-import ApiCall from "../config/index.js"
+import ApiCall from "../config/index.js";
 
 function AllProduct() {
-  const [products,setProducts]=useState([])
-  const fetchProduct =async()=>{
-    try{
-      const res =await ApiCall("/objects","GET")
+  const [products, setProducts] = useState([]);
+  const fetchProduct = async () => {
+    try {
+      const res = await ApiCall("/objects", "GET");
       console.log(res.data);
-      setProducts(res.data)
-    }
-    catch(err){
+      setProducts(res.data);
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
   useEffect(()=>{
     fetchProduct()
   }, [])
   return (
     <div>
       <Sidebar />
+      <div className="border-2 max-w-7xl ml-auto m-6">
+        {products.map((product,index)=>{
+          <div>
+            <h1>{product.name}</h1>
+          </div>
+        })}
+      </div>
     </div>
   );
 }
